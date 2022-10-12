3b69a7145019788586440691e1350f45b7b83c8c
Alexandros <alexandr.kyriakou@gmail.com>
2022-10-12 20:51:59 +0300

    initialize videos section

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,5 +1,6 @@
 import { Box, Stack, Typography } from "@mui/material";
 import Sidebar from "./Sidebar";
+import Videos from "./Videos";
 
 function Feed() {
   return (
@@ -14,6 +15,7 @@ function Feed() {
           borderRight: "1px solid #9b8bcb",
           px: { sx: 0, md: 2 },
           backgroundColor: "#94e4ec",
+          p: 2,
         }}
       >
         <Sidebar />
@@ -25,6 +27,19 @@ function Feed() {
         >
           Copyright 2022 koukos
         </Typography>
+      </Box>
+      {/* Videos section */}
+      <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+        <Typography
+          variant="h4"
+          fontWeight="bold"
+          mb={2}
+          sx={{ color: "white" }}
+        >
+          New <span style={{ color: "#94e4ec" }}>videos</span>
+        </Typography>
+
+        <Videos />
       </Box>
     </Stack>
   );
